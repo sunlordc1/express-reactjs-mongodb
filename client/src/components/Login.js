ec2b481fd31d2f1477000cee6445aa511f0bd562
sunlordc1 <sunlordc1@gmail.com>
2021-10-15 11:14:30 +0700

    update

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,19 +1,42 @@
-import React from "react";
-
+import React, {useState} from "react";
+import axios from 'axios'
 const Login = ()=>{
-  // const [loginState, setLoginState] = useState(['login','register']);
+  const [loginState, setLoginState] = useState({username:'',password:''});
+  const {username,password} = loginState
+  const change= event =>{
+      setLoginState({...loginState, [event.target.name]:event.target.value});
+  }
+  const submit = event=>{
+    event.preventDefault();
+    const login = async ()=>{
+      try{
+          const res = await axios.post(
+              'http://localhost:5000/login',
+              {username,password}
+          )
+          console.log(res.data)
+      }catch(error){
+          console.log(error.message)
+      }
+    }
+    login()
+
+  }
 
   return (
  
-    <form>
+    <form onSubmit={submit}>
+      <p>username: userclone1</p>
+      <p>password: userclone</p>
+      
       <img className="mb-4" src="https://getbootstrap.com/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width={72} height={57} />
       <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
       <div className="form-floating">
-        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+        <input type="text" className="form-control" name="username"  value={username} onChange={change} placeholder="name@example.com" />
         <label htmlFor="floatingInput">Email address</label>
       </div>
       <div className="form-floating">
-        <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+        <input type="password" className="form-control" name="password"  value={password} onChange={change} placeholder="Password" />
         <label htmlFor="floatingPassword">Password</label>
       </div>
       <div className="checkbox mb-3">
